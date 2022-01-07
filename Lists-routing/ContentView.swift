630f5a72c9e8c5bfddb59814985f104001cc0248
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-01-07 11:16:06 +0100

    first commit

---
+++ Lists-routing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lists-routing
+//
+//  Created by Amjad Oudeh on 07.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lists-routing/Lists_routingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lists_routingApp.swift
+//  Lists-routing
+//
+//  Created by Amjad Oudeh on 07.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Lists_routingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
