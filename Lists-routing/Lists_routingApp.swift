d43a910c76df4f92ea02ca5240b737459484fa94
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-01-08 00:36:36 +0100

    Used	 environmentalObject to keep everything on sync when any changes happen!

--- Lists-routing/Lists_routingApp.swift
+++ Lists-routing/Lists_routingApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct Lists_routingApp: App {
+    // @StateObject is a property wrapper, it's responsible to keep the object alive throughout the life of our app!
+    // to keep the editing on sync in different pages
+    @StateObject var order = Order()
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(order)
         }
     }
 }
