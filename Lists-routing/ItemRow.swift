6e1c5190e61773000cecef925fd69fc24ad4df07
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-01-07 14:39:00 +0100

    added dictionary for simples and Colours to show in the screen, then we don't have to style each one alone -> each String = Color. playing with the ItemRow style.

--- Lists-routing/ItemRow.swift
+++ Lists-routing/ItemRow.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ItemRow: View {
+    // dictionary for simples and Colours to show in the screen, then we don't have to style each one alone
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     
     let item: MenuItem
     
     var body: some View {
         HStack{
+            // let's style it a bit
             Image(item.thumbnailImage)
+            // we use clipshape to change the shape of it
+                .clipShape(Circle())
+            // overlady to make the border clear to see
+                .overlay(Circle().stroke(Color.gray, lineWidth: 1.7))
             VStack(alignment: .leading) {
-                Text(item.name)
+                Text(item.name).font(.headline)
                 Text("$\(item.price)")
             }
         }
