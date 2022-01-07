25a71e1d1981ec0617ec3bbb3e80512df45ac4b2
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-01-07 12:07:22 +0100

    Added Assets for the menu app

--- Lists-routing/ContentView.swift
+++ Lists-routing/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
+            // static list View
             List {
                 Text("Backend")
                 Text("Web")
