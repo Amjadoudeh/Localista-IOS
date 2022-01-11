d6a54c841b98e9bd0ca1abbd807aaedd1c684b4d
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-01-12 00:21:32 +0100

    Calculated the total price :)

--- Lists-routing/CheckoutView.swift
+++ Lists-routing/CheckoutView.swift
@@ -18,6 +18,16 @@ struct CheckoutView: View {
     
     let tipAmounts = [0, 10, 15, 20, 25]
     
+    var totalPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+    
+        return formatter.string(from: NSNumber (value: total + tipValue)) ?? "$0"
+    }
+    
     var body: some View {
         Form {
             Section {
@@ -46,7 +56,7 @@ struct CheckoutView: View {
                 .pickerStyle(SegmentedPickerStyle())
             }
             
-            Section(header: Text("TOTAL: 100$ ")) {
+            Section(header: Text("TOTAL: \(totalPrice)")) {
                 Button("Confirm order") {
                     // place order
                 }
