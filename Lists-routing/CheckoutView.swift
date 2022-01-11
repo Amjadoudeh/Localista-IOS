dd781c9835c49d4b78a1335aa1c34bfdf9920abf
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-01-11 23:50:53 +0100

    added a toggle button for loyalty card and text filed to add the card number

--- Lists-routing/CheckoutView.swift
+++ Lists-routing/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @EnvironmentObject var order: Order
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    
     
     let paymentTypes = ["Cash", "Credit Card", "PayPal"]
     
@@ -22,6 +25,12 @@ struct CheckoutView: View {
                         Text($0)
                     }
                 }
+                
+                Toggle("Add Localista loyalty card",
+                        isOn:$addLoyaltyDetails)
+                TextField("Enter your Localista ID",
+                          text: $loyaltyNumber)
+                
                 
             }
         }
