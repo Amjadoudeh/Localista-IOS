450a553a03d791a686b2e69e714c6d64d66e879a
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-01-23 00:03:31 +0100

    changed the name of the app

--- Lists-routing/CreatePlaylistView.swift
+++ Lists-routing/CreatePlaylistView.swift
@@ -11,20 +11,55 @@ struct CreatePlaylistView: View {
     @Environment(\.presentationMode) var presentationMode
     @State private var makeThisProfilePublic = false
     @State private var myPlayList = ""
-    
+
     var body: some View {
-        
-        NavigationView {
-            Text("here you go")
-                .navigationBarTitle("Master view", displayMode: .inline)
-            .navigationBarItems(trailing:
-                HStack {
-                
-                Spacer()
-                Button("save"){}
-                }
-            )
+        Text("Emily").padding().foregroundColor(.yellow)
+    }
+//     VStack {
+//           HStack{
+//                Button("Cancel"){
+//                   presentationMode.wrappedValue.dismiss()
+//               }
+//                Spacer(minLength: 2)
+//               Text("Create playlist")
+//                   .font(.largeTitle)
+//               Spacer(minLength: 20)
+//              Button("save"){
+//                   // TODO: implement the logic to save the new playlist
+//                }
+//          }
+//            HStack{
+//              TextField("Playlist title",
+//                         text: $myPlayList)
+//                    .buttonBorderShape(.automatic)
+//            }
+//           .padding()
+//
+//            HStack{
+//               Toggle("Make this profile public",isOn:$makeThisProfilePublic )
+//           }
+//            .padding()
+//           Spacer()
+//       }
+
+    
+
+
+    struct CreatePlaylistView_Previews: PreviewProvider {
+        static var previews: some View {
+            CreatePlaylistView()
         }
+    }
+}
+
+//import SwiftUI
+//
+//struct CreatPlaylistSheetView: View {
+//    @Environment(\.presentationMode) var presentationMode
+//    @State private var makeThisProfilePublic = false
+//    @State private var myPlayList = ""
+//
+//    var body: some View {
 //
 //        VStack {
 //            HStack{
@@ -42,7 +77,7 @@ struct CreatePlaylistView: View {
 //            HStack{
 //                TextField("Playlist title",
 //                          text: $myPlayList)
-//                    .buttonBorderShape(.automatic)
+//
 //            }
 //            .padding()
 //
@@ -53,59 +88,12 @@ struct CreatePlaylistView: View {
 //            Spacer()
 //        }
 //
-    }
-    
-    
-    struct CreatePlaylistView_Previews: PreviewProvider {
-        static var previews: some View {
-            CreatePlaylistView()
-        }
-    }
-}
-
-//import SwiftUI
+//    }
+//}
 //
-//struct CreatPlaylistSheetView: View {
-//    @Environment(\.presentationMode) var presentationMode
-//    @State private var makeThisProfilePublic = false
-//    @State private var myPlayList = ""
-//    
-//    var body: some View {
-//        
-//                VStack {
-//                    HStack{
-//                        Button("Cancel"){
-//                            presentationMode.wrappedValue.dismiss()
-//                        }
-//                        Spacer(minLength: 2)
-//                        Text("Create playlist")
-//                            .font(.largeTitle)
-//                        Spacer(minLength: 20)
-//                        Button("save"){
-//                            // TODO: implement the logic to save the new playlist
-//                        }
-//                    }
-//                    HStack{
-//                        TextField("Playlist title",
-//                                  text: $myPlayList)
-//        
-//        
-//                    }
-//                    .padding()
-//        
-//                    HStack{
-//                        Toggle("Make this profile public",isOn:$makeThisProfilePublic )
-//                    }
-//                    .padding()
-//                    Spacer()
-//                }
-//        
-//            }
+//struct CreatePlaylistView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CreatPlaylistSheetView()
 //    }
-//    
-//    struct CreatePlaylistView_Previews: PreviewProvider {
-//        static var previews: some View {
-//            CreatPlaylistSheetView()
-//        }
-//    }
+//}
 //
--- Lists-routing/Loclista.swift
+++ Lists-routing/Loclista.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct Lists_routingApp: App {
+struct Loclista: App {
     // @StateObject is a property wrapper, it's responsible to keep the object alive throughout the life of our app!
     // to keep the editing on sync in different pages
     @StateObject var order = Order()
